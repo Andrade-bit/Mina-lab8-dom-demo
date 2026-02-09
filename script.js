af06a39521572bcb48ebbb22a7f38fa0c1ee50ef
Andrade-bit <a.mina.556205@umindanao.edu.ph>
2026-02-09 11:37:13 +0800

    first commt

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
-//include it here all the ids
-const title = document.getElementById("title");
+// Get elements
+const form = document.getElementById("demoForm");
 const Nameinput = document.getElementById("Nameinput");
 const ageinput = document.getElementById("ageinput");
 const output = document.getElementById("output");
 
-//query
-const description = document.querySelector("#description");
-const btnclick = document.querySelector("#btnclick");
+// Use form submit event instead of button click
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
 
-//event handler
-btnclick.addEventListener("click", function(){
-    btnclick.textContent = "clicked!";
+    const name = Nameinput.value.trim();
+    const age = Number(ageinput.value);
 
-    let name = Nameinput.value;
-    let age = Number(ageinput.value);
 
-    output.innerHTML = '<p style="color:black;">Your Name is: ' + name + '</p>';
-    //loose equality 
-    if(age >= 18){
-        output.innerHTML += '<p style="color:green;">You are an adult.</p>';
-    } else {
-        output.innerHTML += '<p style="color:red;">You are a minor!!!.</p>';
-    }
+    output.innerHTML = "";
 
-    //Strict equality
-    if(age === 18){
-        output.innerHTML += '<p style="color:green;">You are exactly 18 years old.</p>';
-    } else {
-        output.innerHTML += '<p style="color:red;">You are not exactly 18 years old.</p>';
-    }
+    // Only proceed if both fields are filled
+    if(name && ageinput.value){
+        output.innerHTML += `<p style="color:black;">Your Name is: ${name}</p>`;
 
-    if(name != ""){
-        output.innerHTML += "<p style = 'color:black;'> Hello ,"+ name + " i am "+ age +" years old" + " </p>"
-    }else{
-        output.innerHTML +=  '<p style="color:red;">Please Enter Your Name!.</p>';
-        
+        if(age >= 18){
+            output.innerHTML += `<p style="color:green;">You are an adult.</p>`;
+        } else {
+            output.innerHTML += `<p style="color:red;">You are a minor!!!</p>`;
+        }
+
+        if(age === 18){
+            output.innerHTML += `<p style="color:green;">You are exactly 18 years old.</p>`;
+        } else {
+            output.innerHTML += `<p style="color:red;">You are not exactly 18 years old.</p>`;
+        }
+
+        output.innerHTML += `<p style="color:black;">Hello ${name}, I am ${age} years old</p>`;
     }
 });
